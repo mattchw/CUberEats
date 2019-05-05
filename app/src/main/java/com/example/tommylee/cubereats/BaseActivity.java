c75744f50716542a443b862faa7966f9886fccf0
hin613 <hin_613@hotmail.com>
2019-05-05 09:45:53 +0800

    bug fix in abstract class baseActivity

--- app/src/main/java/com/example/tommylee/cubereats/BaseActivity.java
+++ app/src/main/java/com/example/tommylee/cubereats/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.example.tommylee.cubereats;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
@@ -8,14 +9,31 @@ import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
 
 public abstract class BaseActivity extends AppCompatActivity {
+    Activity curActivity;
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
         switch (item.getItemId()) {
+            case R.id.home:
+                curActivity = this;
+                if(null != curActivity){
+                    if(!(curActivity instanceof RestaurantListActivity)){
+                        Intent homeIntent = new Intent(this, RestaurantListActivity.class);
+                        finish();
+                        startActivity(homeIntent);
+                    }
+                }
+                return true;
+
             case R.id.delivery:
-                Intent deliveryIntent = new Intent(this, OrderDeliveryListActivity.class);
-                finish();
-                startActivity(deliveryIntent);
+                curActivity = this;
+                if(null != curActivity){
+                    if(!(curActivity instanceof RestaurantListActivity)){
+                        Intent deliveryIntent = new Intent(this, OrderDeliveryListActivity.class);
+                        finish();
+                        startActivity(deliveryIntent);
+                    }
+                }
                 return true;
 
             case R.id.logout:
