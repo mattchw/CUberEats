32a399aa73f65fa15a87d53a1dcbf68dc962355b
tomilia <koekax@gmail.com>
2019-05-05 17:46:27 +0800

    add to order: check order already in

--- app/src/main/java/com/example/tommylee/cubereats/Detail_Inner_Activity.java
+++ app/src/main/java/com/example/tommylee/cubereats/Detail_Inner_Activity.java
@@ -12,17 +12,29 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.SetOptions;
 import com.google.firebase.firestore.Source;
 import com.google.firebase.firestore.model.Document;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class Detail_Inner_Activity extends AppCompatActivity {
     ImageView imageView;
@@ -35,7 +47,7 @@ public class Detail_Inner_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_detail__inner_);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         Bundle extras = getIntent().getExtras();
-
+        isAddedToCartAlready();
         tv = findViewById(R.id.quantity);
 
         if (extras != null) {
@@ -48,6 +60,66 @@ public class Detail_Inner_Activity extends AppCompatActivity {
 
 
     }
+    private void isAddedToCartAlready()
+    {
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        final Query colRef = db.collection("order").whereArrayContains("mealID",id);
+        colRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    if(!task.getResult().isEmpty()) {
+                        Log.d("skdog","already in!");
+                    }
+                    else
+                    {
+                        Log.d("tagx","not in!");
+                    }
+                }
+            }
+        });
+    }
+    private void addExist(FirebaseFirestore db,String path){
+        final Map<String, Object> order = new HashMap<>();
+        ArrayList mealId = new ArrayList<String>();
+        mealId.add(id);
+        order.put("mealID",mealId);
+
+        final DocumentReference ref=db.collection("order").document(path);
+
+                ref.update("mealID",FieldValue.arrayUnion(id));
+
+
+
+
+    }
+    private void initOrder(FirebaseFirestore db){
+        Map<String, Object> order = new HashMap<>();
+        ArrayList mealId = new ArrayList<String>();
+        mealId.add(id);
+        order.put("customerCoordinate",new GeoPoint(0,0));
+        order.put("customerID",FirebaseAuth.getInstance().getCurrentUser().getUid());
+        order.put("driverCoordinate",new GeoPoint(0,0));
+        order.put("driverID","");
+        order.put("mealID",mealId);
+        order.put("isPaid",false);
+        db.collection("order").add(order)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("written", "DocumentSnapshot written with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("fail", "Error adding document", e);
+                    }
+                });
+
+
+
+    }
     public void AddToCart(View v)
     {
         /*
@@ -57,7 +129,60 @@ public class Detail_Inner_Activity extends AppCompatActivity {
         spinner?
         finishforactivityresult
          */
-        finishActivity(1);
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Source source = Source.CACHE;
+
+        final Query colRef = db.collection("order").whereEqualTo("customerID", FirebaseAuth.getInstance().getCurrentUser().getUid());
+        colRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    if(task.getResult().isEmpty())
+                    {
+                        //create new order;
+                        initOrder(db);
+                    }
+                    else
+                    {
+                        for(DocumentSnapshot qds:task.getResult())
+                        {
+
+                            addExist(db,qds.getId());
+                            /*Map<String, Object> order = new HashMap<>();
+                            ArrayList mealId = new ArrayList<String>();
+                            mealId.add(id);
+                            order.put("customerCoordinate",new GeoPoint(0,0));
+                            order.put("customerID",FirebaseAuth.getInstance().getCurrentUser().getUid());
+                            order.put("driverCoordinate",new GeoPoint(0,0));
+                            order.put("driverID","wefokwo");
+                            order.put("mealID",mealId);
+                            order.put("isPaid",false);
+
+                            Boolean sus=db.collection("order").document(qds.getId()).set(order).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d("qdsref","sfasf");
+                                }
+                            })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.w("fail", "Error adding document", e);
+                                        }
+                                    }).isSuccessful();
+                                    */
+                        }
+
+                        //add to exist order
+                    }
+
+                    //initAdapter();
+                } else {
+                    Log.d("dkcx", "Error getting documents: ", task.getException());
+                }
+            }
+        });
+        finish();
     }
     public void minusSelection(View v)
     {
