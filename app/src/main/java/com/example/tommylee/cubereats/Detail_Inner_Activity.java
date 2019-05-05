e7a07378baa973804dcb241a826a8cc21345a07b
tomilia <koekax@gmail.com>
2019-05-05 18:12:08 +0800

    merge

--- app/src/main/java/com/example/tommylee/cubereats/Detail_Inner_Activity.java
+++ app/src/main/java/com/example/tommylee/cubereats/Detail_Inner_Activity.java
@@ -57,7 +57,7 @@ public class Detail_Inner_Activity extends AppCompatActivity {
         spinner?
         finishforactivityresult
          */
-        finishActivity(1);
+        finish();
     }
     public void minusSelection(View v)
     {
